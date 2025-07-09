d396eedb493b367916af8b55fd07c1a9e40868b4
Nurdin027 <amarnurdin000@gmil.com>
2025-07-09 09:06:02 +0700

    fore-dynamic

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -4,19 +4,21 @@ import Banner from "@/components/banner";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
+export const dynamic = 'force-dynamic'
 export const revalidate = 0;
 
 const HomePage = async () => {
-  const products = await getProducts({ isFeatured: true });
+  const products = await getProducts({isFeatured: true});
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <div className="flex align-center justify-center text-center" style={{ padding: "100px",backgroundColor: "grey" }}>
+        <div className="flex align-center justify-center text-center"
+             style={{padding: "100px", backgroundColor: "grey"}}>
           <a className="font-bold text-3xl sm:text-5xl lg:text-6xl">FATIH PARFUM</a>
         </div>
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-          <ProductList title="Produk Unggulan" items={products} />
+          <ProductList title="Produk Unggulan" items={products}/>
         </div>
       </div>
     </Container>
